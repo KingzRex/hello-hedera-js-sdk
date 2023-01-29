5007c0a3ef440f49374f45c7c7682be3d4195454
KingzRex <toochukwukingz6@gmail.com>
2023-01-29 23:14:05 +0000

    all good now, ID, transcation and balance setup

--- index.js
+++ index.js
@@ -1,81 +1,70 @@
-const {
-  Client,
-  PrivateKey,
-  AccountCreateTransaction,
-  AccountBalanceQuery,
-  Hbar,
-} = require("@hashgraph/sdk");
+const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction} = require("@hashgraph/sdk");
 require("dotenv").config();
 
-//const client = Client.forTestnet();
-//client.setOperator(myAccountId, myPrivateKey);
-//-----------------------<enter code below>--------------------------------------
+async function main() {
 
-//Create new keys
-const newAccountPrivateKey = PrivateKey.generateED25519();
-const newAccountPublicKey = newAccountPrivateKey.publicKey;
+    //Grab your Hedera testnet account ID and private key from your .env file
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
-//Create a new account with 1,000 tinybar starting balance
-const newAccount = await new AccountCreateTransaction()
-  .setKey(newAccountPublicKey)
-  .setInitialBalance(Hbar.fromTinybars(1000))
-  .execute(client);
+    // If we weren't able to grab it, we should throw a new error
+    if (myAccountId == null ||
+        myPrivateKey == null ) {
+        throw new Error("Environment variables myAccountId and myPrivateKey must be present");
+    }
 
-  //console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
-//-----------------------<enter code below>--------------------------------------
+    // Create our connection to the Hedera network
+    // The Hedera JS SDK makes this really easy!
+    const client = Client.forTestnet();
 
-//Create the transfer transaction
-const sendHbar = await new TransferTransaction()
-.addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000)) //Sending account
-.addHbarTransfer(newAccountId, Hbar.fromTinybars(1000)) //Receiving account
-.execute(client);
+    client.setOperator(myAccountId, myPrivateKey);
 
-//Verify the transaction reached consensus
-const transactionReceipt = await sendHbar.getReceipt(client);
-console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+    //Create new keys
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
-//Request the cost of the query
-const queryCost = await new AccountBalanceQuery()
+    //Create a new account with 1,000 tinybar starting balance
+    const newAccountTransactionResponse = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccountTransactionResponse.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+
+    console.log("The new account ID is: " +newAccountId);
+
+    //Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybars.");
+
+    //Create the transfer transaction
+    const sendHbar = await new TransferTransaction()
+        .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
+        .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
+        .execute(client);
+
+    //Verify the transaction reached consensus
+    const transactionReceipt = await sendHbar.getReceipt(client);
+    console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+    
+    //Request the cost of the query
+    const queryCost = await new AccountBalanceQuery()
      .setAccountId(newAccountId)
      .getCost(client);
 
-console.log("The cost of query is: " +queryCost);
+    console.log("The cost of query is: " +queryCost);
 
+    //Check the new account's balance
+    const getNewBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
 
-// Get the new account ID
-const getReceipt = await newAccount.getReceipt(client);
-const newAccountId = getReceipt.accountId;
+    console.log("The account balance after the transfer is: " +getNewBalance.hbars.toTinybars() +" tinybars.")
 
-//Log the account ID
-console.log("The new account ID is: " + newAccountId);
-
-//Verify the account balance
-const accountBalance = await new AccountBalanceQuery()
-  .setAccountId(newAccountId)
-  .execute(client);
-
-console.log(
-  "The new account balance is: " +
-    accountBalance.hbars.toTinybars() +
-    " tinybar."
-);
-
-async function main() {
-  //Grab your Hedera testnet account ID and private key from your .env file
-  const myAccountId = process.env.MY_ACCOUNT_ID;
-  const myPrivateKey = process.env.MY_PRIVATE_KEY;
-
-  // If we weren't able to grab it, we should throw a new error
-  if (!myAccountId || !myPrivateKey) {
-    throw new Error(
-      "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
-    );
-  }
-
-  // Create our connection to the Hedera network
-  // The Hedera JS SDK makes this really easy!
-  const client = Client.forTestnet();
-
-  client.setOperator(myAccountId, myPrivateKey);
 }
-main().catch((err) => console.error(err));
+main();
