1498a1ec05a478ccd4c5bb753fc2e519b9341a92
KingzRex <toochukwukingz6@gmail.com>
2023-01-29 22:32:31 +0000

    create and setup hedera account

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const { Client } = require("@hashgraph/sdk");
+const {
+  Client,
+  PrivateKey,
+  AccountCreateTransaction,
+  AccountBalanceQuery,
+  Hbar,
+} = require("@hashgraph/sdk");
 require("dotenv").config();
+
+//const client = Client.forTestnet();
+//client.setOperator(myAccountId, myPrivateKey);
+//-----------------------<enter code below>--------------------------------------
+
+//Create new keys
+const newAccountPrivateKey = PrivateKey.generateED25519();
+const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+//Create a new account with 1,000 tinybar starting balance
+const newAccount = await new AccountCreateTransaction()
+  .setKey(newAccountPublicKey)
+  .setInitialBalance(Hbar.fromTinybars(1000))
+  .execute(client);
+
+// Get the new account ID
+const getReceipt = await newAccount.getReceipt(client);
+const newAccountId = getReceipt.accountId;
+
+//Log the account ID
+console.log("The new account ID is: " + newAccountId);
+
+//Verify the account balance
+const accountBalance = await new AccountBalanceQuery()
+  .setAccountId(newAccountId)
+  .execute(client);
+
+console.log(
+  "The new account balance is: " +
+    accountBalance.hbars.toTinybars() +
+    " tinybar."
+);
 
 async function main() {
   //Grab your Hedera testnet account ID and private key from your .env file
@@ -19,4 +57,4 @@ async function main() {
 
   client.setOperator(myAccountId, myPrivateKey);
 }
-main().catch(err => console.error(err));
+main().catch((err) => console.error(err));
